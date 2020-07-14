e6adfe3e57f61ba603f274d11a9d45c00cfc8b67
bella-cockrell <heybellac@gmail.com>
2020-07-14 21:47:19 +0100

    Amending models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const exerciseSchema = new Schema(
+const userSchema = new Schema(
   {
-    username: { type: String, required: true },
-    description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, required: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
   },
   {
     timestamps: true,
   }
 )
 
-const Exercise = mongoose.model('Exercise', exerciseSchema)
+const User = mongoose.model('User', userSchema)
 
-module.exports = Exercise
+module.exports = User
