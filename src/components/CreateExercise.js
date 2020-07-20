7a58fdfdb1f15a4b4558aebce1f60ad7b772dbe0
bella-cockrell <heybellac@gmail.com>
2020-07-20 18:19:51 +0100

    Adding state and methods

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CreateExercise = () => {
-  return <div> Create Exercise!</div>
+const [exercise, setExercise] = useState({
+  username: '',
+  description: '',
+  duration: 0,
+  date: new Date(),
+  users: [],
+})
+
+const onChangeUsername = (e) => {
+  setExercise({ username: e.target.value })
+}
+
+const onChangeDescription = (e) => {
+  setExercise({ description: e.target.value })
+}
+
+const onChangeDuration = (e) => {
+  setExercise({ duration: e.target.value })
+}
+
+// const onChangeDate = (e) => {
+//   setExercise({ date: date })
+// }
+
+const onSubmit = (e) => {
+  e.preventDefault()
+  const submittedExercise = {
+    username: exercise.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date,
+  }
+  console.log(submittedExercise)
+  window.location = '/'
+}
+
+const CreateExercise = (props) => {
+  return <div>{exercise}</div>
 }
 
 export default CreateExercise
