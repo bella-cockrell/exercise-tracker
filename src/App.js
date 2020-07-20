6500d85425946ec71f8995700fafef62392c9ef0
bella-cockrell <heybellac@gmail.com>
2020-07-20 10:36:34 +0100

    Adding Routes for frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+
+import './components/Navbar'
+import './components/ExerciseList'
+import './components/EditExercise'
+import './components/CreateExercise'
+import './components/CreateUser'
 
 function App() {
   return (
-  <div className="container">
-    hello world
-  </div>
-  )}
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
+  )
+}
 
 export default App
