99ecf2839420702fee5ccbae84f022e012184f29
bella-cockrell <heybellac@gmail.com>
2020-07-20 11:58:57 +0100

    Adding imports

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Navbar from './components/Navbar'
-// import './components/ExerciseList'
-// import './components/EditExercise'
-// import './components/CreateExercise'
-// import './components/CreateUser'
+import ExerciseList from './components/ExerciseList'
+import EditExercise from './components/EditExercise'
+import CreateExercise from './components/CreateExercise'
+import CreateUser from './components/CreateUser'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
-      {/* <Route path="/" exact component={ExerciseList} />
+      <Route path="/" exact component={ExerciseList} />
       <Route path="/edit/:id" exact component={EditExercise} />
       <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} /> */}
+      <Route path="/user" exact component={CreateUser} />
     </Router>
   )
 }
