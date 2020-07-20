15b2203411ddfef3331da0ba31b9147917fd3de8
bella-cockrell <heybellac@gmail.com>
2020-07-20 11:04:06 +0100

    Adding Navbar component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './components/Navbar'
-import './components/ExerciseList'
-import './components/EditExercise'
-import './components/CreateExercise'
-import './components/CreateUser'
+import Navbar from './components/Navbar'
+// import './components/ExerciseList'
+// import './components/EditExercise'
+// import './components/CreateExercise'
+// import './components/CreateUser'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
-      <Route path="/" exact component={ExerciseList} />
+      {/* <Route path="/" exact component={ExerciseList} />
       <Route path="/edit/:id" exact component={EditExercise} />
       <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} />
+      <Route path="/user" exact component={CreateUser} /> */}
     </Router>
   )
 }
